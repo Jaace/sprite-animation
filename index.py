ae94538abe5606d70d25c97954c00fb5dabdf3dc
Jason Boyle <jb5531@gmail.com>
2023-11-04 12:15:01 -0400

    Images loading, no animation yet

--- index.py
+++ index.py
@@ -1,10 +1,21 @@
-import pygame, sys
+import pygame, sys, os
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__()
-        self.image = pygame.Surface([20, 20])
-        self.image.fill((255, 255, 255))
+        self.sprites = []
+
+        # Outside of this tutorial, probably best to use a sprite sheet...
+        directory = './frog'
+        for filename in os.listdir(directory):
+            f = os.path.join(directory, filename)
+            if os.path.isfile(f):
+                image = pygame.image.load(f)
+                self.sprites.append(image)
+
+        self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
+
         self.rect = self.image.get_rect()
         self.rect.topleft = [pos_x, pos_y]
 
@@ -17,7 +28,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Sprite Animation')
 
 moving_sprites = pygame.sprite.Group()
-player = Player(100, 100)
+player = Player(10, 10)
 moving_sprites.add(player)
 
 while True:
@@ -30,4 +41,3 @@ while True:
     moving_sprites.draw(screen)
     pygame.display.flip()
     clock.tick(60)
-    
